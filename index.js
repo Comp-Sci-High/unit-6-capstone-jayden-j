1d84db253f2702816763697efa8bf1ca2d9a9a8d
JayJames48 <jayden.james25@compscihigh.org>
2025-05-16 14:26:22 +0000

    not done

--- index.js
+++ index.js
@@ -18,29 +18,62 @@ const journalSchema = new mongoose.Schema(
     {
         name: { type: String, required: true },
         issue: { type: String, required: true },
-        recipient: { type: String, rquired: true },
+        recipient: { type: String, required: true }
+
     }
 );
 
-const Journal = mongoose.model("Journal", JournalSchema, "Journal");
+const Journal = mongoose.model("Journal", journalSchema, "Journal");
 
 app.get("/", async (req, res) => {
     const students = await Journal.find({});
     res.json({ students });
 });
 
-app.delete("/delte/journal/id")
-
-app.patch("/journal/:name",async(req,res)=>{
-    const res = await journalSchema.findoneAndUpdate({
-name:req.params.name},
-{req.body},{new:true})
- res.json(response);   
+app.delete("/journal/:id", async (req, res) => {
+    
 });
 
+
+
+
+app.patch("/journal/:name",async(req,res)=>{  
+const result = await Journal.findOneAndUpdate(
+    { name: req.params.name },
+    req.body,
+    { new: true }
+);
+res.json(result);
+})
 app.post("/user/save",async(req,res)=>{
  const user1 = await new user({
 name:req.body.name,
 issue:req.body.name,
 recipient:req.body.name
 });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function startServer(){
+ await mongoose.connect("mongodb+srv://SE12:CSH2025@cluster0.eixzx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+ app.listen(3000, () => {
+        console.log(`Server running.`);
+    });
+}
+startServer();
